b8cf879bb38d202a5ef5d4776f38777883c2f1be
Mike Gulline <mike@mikegulline.com>
2023-01-10 15:29:38 -0800

    added header and footer

---
+++ components/header/Ad.jsx
@@ -0,0 +1,3 @@
+export default function Ad() {
+  return <div>advertisment</div>;
+}
---
+++ components/header/index.jsx
@@ -0,0 +1,13 @@
+import Ad from './Ad';
+import styles from './styles.module.scss';
+
+export default function Header() {
+  return (
+    <>
+      <Ad />
+      <header>
+        <h1>next header</h1>
+      </header>
+    </>
+  );
+}
