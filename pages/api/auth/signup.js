e557e99ce46bca9d6762abdd33619a93ea92cc32
Mike Gulline <mike@mikegulline.com>
2023-01-12 13:08:22 -0800

    udated signup auth

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,7 @@
 import nc from 'next-connect';
 import db from '../../../utils/db';
-
+import { validateEmail } from '../../../utils/validation';
+import User from '../../../models/User';
 const handler = nc();
 
 handler.post(async (req, res) => {
@@ -10,7 +11,18 @@ handler.post(async (req, res) => {
     if (!name || !email || !password) {
       return res.status(400).json({ message: 'Please fill in all fields.' });
     }
-    res.status(200).json(req.body);
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: 'Invalid email.' });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: `The email ${email} has already been registered.`,
+      });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
