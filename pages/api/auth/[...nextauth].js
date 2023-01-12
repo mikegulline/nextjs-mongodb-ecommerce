1dcdafd87c969d71451229a228ce05f90a54093b
Mike Gulline <mike@mikegulline.com>
2023-01-11 18:12:11 -0800

    added Fromik and yup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,4 @@
 import NextAuth from 'next-auth';
-import TwitterProvider from 'next-auth/providers/twitter';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
@@ -32,14 +31,6 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
@@ -50,9 +41,9 @@ export const authOptions = {
       issuer: process.env.AUTH0_ISSUER,
     }),
   ],
-  // pages: {
-  //   signIn: '/signin',
-  // },
+  pages: {
+    signIn: '/signin',
+  },
   session: {
     strategy: 'jwt',
   },
--- pages/signin.js
+++ pages/signin.js
@@ -1,7 +1,16 @@
+import { Formik, Form } from 'formik';
+
 export default function SignIn() {
   return (
     <>
       <h1>Sign In</h1>
+      <Formik>
+        {(form) => (
+          <Form>
+            <input />
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
