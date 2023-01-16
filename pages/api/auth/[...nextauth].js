e4a3812f26463193bbfab227217ae50c89ea5077
Mike Gulline <mike@mikegulline.com>
2023-01-16 12:21:00 -0800

    added session callbacks to nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -45,6 +45,14 @@ export const authOptions = {
       issuer: process.env.AUTH0_ISSUER,
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      let user = await User.findById(token.sub);
+      session.user._id = token.sub || user._id.toString();
+      session.user.role = user.role || 'user';
+      return session;
+    },
+  },
   pages: {
     signIn: '/signin',
   },
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 // import styles from '../styles/Home.module.scss';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <>
       <h1>next home</h1>
