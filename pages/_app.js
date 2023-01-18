fead191f192e68872552e49fa54763823e797802
Mike Gulline <mike@mikegulline.com>
2023-01-18 10:48:34 -0800

    created a store wrapper to free up _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
 import Head from 'next/head';
-import { SessionProvider } from 'next-auth/react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-import store from '../store';
-
-import '../styles/globals.scss';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import StoreWrapper from '../store';
 
-let persistor = persistStore(store);
+import '../styles/globals.scss';
 
 export default function App({
   Component,
@@ -23,16 +17,11 @@ export default function App({
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-
-      <SessionProvider session={session}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Header />
-            <Component {...pageProps} />
-            <Footer />
-          </PersistGate>
-        </Provider>
-      </SessionProvider>
+      <StoreWrapper session={session}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </StoreWrapper>
     </>
   );
 }
