e9d9f2c7c6717a73186df11693207ad27b0025c9
Mike Gulline <mike@mikegulline.com>
2023-01-18 14:37:18 -0800

    removed unused items.

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,10 +1,19 @@
-import Top from './TOP';
+import { useSelector } from 'react-redux';
+import SignInSighOut from '../inputs/login-btn';
 
 export default function Header() {
+  const { cart } = useSelector((state) => ({ ...state }));
   return (
     <header>
-      <Top />
       <h1>next header</h1>
+      <ul>
+        <li>
+          <span>Cart</span> <span>{cart.length}</span>
+        </li>
+        <li>
+          <SignInSighOut />
+        </li>
+      </ul>
     </header>
   );
 }
